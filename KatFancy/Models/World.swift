242fb20aab51ffc699c4157041d5d4db150b16e0
vermont42 <vermontcoder@gmail.com>
2023-01-26 13:31:38 -0800

    simplify World

--- KatFancy/Models/World.swift
+++ KatFancy/Models/World.swift
@@ -12,13 +12,11 @@ var Current = World.chooseWorld()
 class World: ObservableObject {
   @Published var settings: Settings
   @Published var soundPlayer: SoundPlayer
-  @Published var getterSetter: GetterSetter
   @Published var imageLoader: ImageLoader
 
-  init(settings: Settings, soundPlayer: SoundPlayer, getterSetter: GetterSetter, imageLoader: ImageLoader) {
+  init(settings: Settings, soundPlayer: SoundPlayer, imageLoader: ImageLoader) {
     self.settings = settings
     self.soundPlayer = soundPlayer
-    self.getterSetter = getterSetter
     self.imageLoader = imageLoader
   }
 
@@ -35,32 +33,82 @@ class World: ObservableObject {
   }
 
   static let device: World = {
-    let getterSetter = UserDefaultsGetterSetter()
     return World(
-      settings: Settings(getterSetter: getterSetter),
+      settings: Settings(getterSetter: UserDefaultsGetterSetter()),
       soundPlayer: RealSoundPlayer(),
-      getterSetter: getterSetter,
       imageLoader: ImageLoader()
     )
   }()
 
   static let simulator: World = {
-    let getterSetter = UserDefaultsGetterSetter()
     return World(
-      settings: Settings(getterSetter: getterSetter),
+      settings: Settings(getterSetter: UserDefaultsGetterSetter()),
       soundPlayer: RealSoundPlayer(),
-      getterSetter: getterSetter,
       imageLoader: ImageLoader()
     )
   }()
 
   static let unitTest: World = {
-    let getterSetter = DictionaryGetterSetter()
     return World(
-      settings: Settings(getterSetter: getterSetter),
+      settings: Settings(getterSetter: DictionaryGetterSetter()),
       soundPlayer: TestSoundPlayer(),
-      getterSetter: getterSetter,
       imageLoader: ImageLoader()
     )
   }()
 }
+
+//class World: ObservableObject {
+//  @Published var settings: Settings
+//  @Published var soundPlayer: SoundPlayer
+//  @Published var getterSetter: GetterSetter
+//  @Published var imageLoader: ImageLoader
+//
+//  init(settings: Settings, soundPlayer: SoundPlayer, getterSetter: GetterSetter, imageLoader: ImageLoader) {
+//    self.settings = settings
+//    self.soundPlayer = soundPlayer
+//    self.getterSetter = getterSetter
+//    self.imageLoader = imageLoader
+//  }
+//
+//  static func chooseWorld() -> World {
+//#if targetEnvironment(simulator)
+//    if NSClassFromString("XCTest") != nil {
+//      return World.unitTest
+//    } else {
+//      return World.simulator
+//    }
+//#else
+//    return World.device
+//#endif
+//  }
+//
+//  static let device: World = {
+//    let getterSetter = UserDefaultsGetterSetter()
+//    return World(
+//      settings: Settings(getterSetter: getterSetter),
+//      soundPlayer: RealSoundPlayer(),
+//      getterSetter: getterSetter,
+//      imageLoader: ImageLoader()
+//    )
+//  }()
+//
+//  static let simulator: World = {
+//    let getterSetter = UserDefaultsGetterSetter()
+//    return World(
+//      settings: Settings(getterSetter: getterSetter),
+//      soundPlayer: RealSoundPlayer(),
+//      getterSetter: getterSetter,
+//      imageLoader: ImageLoader()
+//    )
+//  }()
+//
+//  static let unitTest: World = {
+//    let getterSetter = DictionaryGetterSetter()
+//    return World(
+//      settings: Settings(getterSetter: getterSetter),
+//      soundPlayer: TestSoundPlayer(),
+//      getterSetter: getterSetter,
+//      imageLoader: ImageLoader()
+//    )
+//  }()
+//}
